f27a0bbacaf03973ba90e39c860b19e78b38a176
GabiBravo2019 <gabravo.2016@gmail.com>
2020-09-24 15:56:03 -0500

    final commit from livestream on June 14

--- apps/lectures/admin.py
+++ apps/lectures/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Lecture
 
 # Register your models here.
+
+class LectureteAdmin(admin.ModelAdmin):
+    list_display = ('title','lecturer_name','date')
+    search_fields = ('title',)
+
+admin.site.register(Lecture,LectureteAdmin)
--- apps/lectures/views.py
+++ apps/lectures/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import serializers, viewsets
+
+from .models import Lecture
 
 # Create your views here.
+
+class LectureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lecture
+        fields = ('id','title','description','lecturer_name',
+                  'date','duration','slides_url','is_require')
+
+class LectureViewSet(viewsets.ModelViewSet):
+    queryset = Lecture.objects.all()
+    serializer_class = LectureSerializer
