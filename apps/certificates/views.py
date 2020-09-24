c68bc2bf2874968420106176d3bc7421973f0eae
GabiBravo2019 <gabravo.2016@gmail.com>
2020-09-24 02:28:07 -0500

    added new apps

--- apps/certificates/views.py
+++ apps/certificates/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import serializers, viewsets
+
+from .models import Certificate
 
 # Create your views here.
+
+class CerticateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
+        fields = ('id','name','description','created_at','update_at')
+
+class CertificateViewSet(viewsets.ModelViewSet):
+    queryset = Certificate.objects.all()
+    serializer_class = CerticateSerializer
--- apps/users/views.py
+++ apps/users/views.py
@@ -9,7 +9,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ('email','password','is_student')
+        fields = ('id','email','password','is_student')
         extra_kwargs = {'password': {'write_only':True,'min_length':8}}
 
     def create(self, validated_data):
